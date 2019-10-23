25864ef3c1ccdd359ab7a21b9f76b5cf181ff949
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-23 16:33:03 -0500

    adding sessions and cookies exercies

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -5,13 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") != null) {
-            request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/login");
-        }
+        request.getSession().invalidate();
+        response.sendRedirect("/login?logout");
     }
 }
--- src/main/webapp/partials/navbar.jsp
+++ src/main/webapp/partials/navbar.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
